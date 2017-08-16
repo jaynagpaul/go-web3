8e02f3b1a0edc5605aff1767bd89d5aebd5298e1
Jay Nagpaul <jaynagpaul@gmail.com>
2017-08-15 23:01:02 -0400

    Spacing

--- doc.go
+++ doc.go
@@ -4,6 +4,7 @@ Package web3 provides a wrapper for ethereum's web3.js for use with gopherjs.
 Getting Started:
 
 $ go get -u github.com/jaynagpaul/go-web3
+
 $ go get -u github.com/gopherjs/gopherjs
 
 	import "github.com/jaynagpaul/go-web3"
